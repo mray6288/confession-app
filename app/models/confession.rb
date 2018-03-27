afa6fe63812bdd23eb5c8d073e3e7a37c73dd144
Ray Blasko <mray6288@gmail.com>
2018-03-27 08:56:15 -0400

    fixed comments nested attr

--- app/models/confession.rb
+++ app/models/confession.rb
@@ -3,7 +3,7 @@ class Confession < ApplicationRecord
 	has_many :topics, through: :confessiontopics
 	has_many :comments
 	belongs_to :user
-	accepts_nested_attributes_for :comment
+	accepts_nested_attributes_for :comments
 
 	def topic_attributes=(topic)
 		new_topic = Topic.find_or_create_by(name: topic.name)
