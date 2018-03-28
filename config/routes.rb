1c624743f9d08db6330105a8fbd65522d14d129f
Ray Blasko <mray6288@gmail.com>
2018-03-28 14:04:29 -0400

    start analytics page

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,8 @@ Rails.application.routes.draw do
 	get '/profile', to: 'users#show', as: 'user'
 	get '/signup',  to: 'users#new', as: 'signup'
 	get '/login', to: 'sessions#new', as: 'login'
-<<<<<<< HEAD
-	delete '/logout', to: 'sessions#destroy', as: 'logout'
-=======
+	get '/analytics', to: 'application#analytics', as: 'analytics'
 	delete 'logout', to: 'sessions#destroy', as: 'logout'
->>>>>>> 6e68e3eee2f8e591f04140dc73cfd11c63c7ffd8
 	post '/sessions', to: 'sessions#create', as: 'sessions'
 	root 'application#home'
 
