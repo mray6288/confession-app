34091ff188f424ff40583f18333c22317e722828
bafab <bafa@tutanota.de>
2018-03-28 13:53:11 -0400

    Visual clean-ups

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,11 @@ Rails.application.routes.draw do
 	resources :topics
 	resources :comments
 	resources :confessions
-
-	resource :users, only: [:create]
+	resource :users
 	get '/profile', to: 'users#show', as: 'user'
 	get '/signup',  to: 'users#new', as: 'signup'
 	get '/login', to: 'sessions#new', as: 'login'
-<<<<<<< HEAD
 	delete '/logout', to: 'sessions#destroy', as: 'logout'
-=======
-	delete 'logout', to: 'sessions#destroy', as: 'logout'
->>>>>>> 6e68e3eee2f8e591f04140dc73cfd11c63c7ffd8
 	post '/sessions', to: 'sessions#create', as: 'sessions'
 	root 'application#home'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
